054e1407b93632401af4336065fd56e0e6e2b70e
booi-dev <booimangang@gmail.com>
2023-08-10 23:19:30 +0530

    Start curve sections

---
+++ src/app/curve-sections/page.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./styles.css";
+
+const page = () => {
+  return (
+    <div className="curve-sections">
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto,
+          esse, necessitatibus temporibus inventore fugiat dolor rem labore
+          repellat ipsam dolore quisquam consequuntur ipsa repellendus, natus
+          provident! Quas dolorum quam temporibus!
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Perspiciatis iure architecto, illo esse voluptatibus recusandae rerum
+          laudantium minus sint veniam ullam! Doloribus quas natus, perspiciatis
+          dolorum voluptas suscipit officiis exercitationem possimus quibusdam,
+          iusto minus accusantium quo. Unde, aperiam!
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Beatae aliquam eius possimus aut eligendi incidunt ratione, ab,
+          dolores vero quibusdam, praesentium molestiae. Sit itaque ipsum, odio
+          ipsam laborum aliquam odit! Corporis veritatis ex molestiae facere
+          voluptate, nemo ducimus!
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Magni, voluptates pariatur ipsa ullam nisi mollitia cum nobis!
+          Voluptas ad totam iure neque, harum sit quasi labore? Ipsa a
+          repudiandae ipsam quo, cumque impedit perspiciatis. Sint repellat
+          autem perferendis?
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Eligendi dolor quidem rem expedita vero id suscipit excepturi aperiam.
+          Molestias debitis soluta quidem? Ipsa perferendis dignissimos,
+          corporis, corrupti alias maxime similique a officiis quas provident
+          iusto sunt consectetur asperiores.
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Dolor sunt, ullam omnis quae itaque accusamus enim voluptate!
+          Voluptate, nulla ipsa quis facere accusantium, ullam saepe vero vel
+          adipisci asperiores nam. Voluptates alias architecto quam incidunt
+          doloremque consequatur excepturi!
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Iusto labore minima recusandae aperiam nulla! Eligendi similique
+          voluptatibus maxime delectus dolores nostrum molestiae repellendus
+          aperiam facere nulla odit accusamus repudiandae dolor tempora nisi
+          esse dignissimos exercitationem, tempore quam autem.
+        </p>
+      </section>
+      <section>
+        <h1>Curves</h1>
+        <p>
+          Fugiat dicta hic earum totam excepturi reprehenderit quod culpa et,
+          perferendis quibusdam ad cumque sit consectetur, repellat rerum est
+          dignissimos, laudantium harum? Suscipit eligendi sunt qui distinctio,
+          quae vitae adipisci?
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24">
       <Link href="/top-progress-bar">top progress bar</Link>
       <Link href="/gradient-animation">gradient animation</Link>
+      <Link href="/curve-sections">curve sections</Link>
     </main>
   );
 }
