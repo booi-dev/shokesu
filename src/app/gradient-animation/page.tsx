3e81051ae42e5608005cfc03cd7a10ce7ccf498b
booi-dev <booimangang@gmail.com>
2023-08-10 22:43:21 +0530

    Create gradient animation

---
+++ src/app/gradient-animation/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Scene = () => {
+  return (
+    <div className="button-container">
+      <button className="button">Hover me</button>
+    </div>
+  );
+};
+
+export default Scene;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24">
       <Link href="/top-progress-bar">top progress bar</Link>
+      <Link href="/gradient-animation">gradient animation</Link>
     </main>
   );
 }
