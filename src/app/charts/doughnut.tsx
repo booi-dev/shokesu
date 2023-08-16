714eadc2f392510318359b14d050736ae53e91f2
booi-dev <booimangang@gmail.com>
2023-08-16 23:51:00 +0530

    Create simple doughnut chart

--- src/app/charts/doughnut.tsx
+++ src/app/charts/doughnut.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
@@ -8,15 +10,15 @@ export const data = {
   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
   datasets: [
     {
-      label: "# of Votes",
+      label: "",
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 99, 132)",
+        "rgba(54, 162, 235)",
+        "rgba(255, 206, 86)",
+        "rgba(75, 192, 192)",
+        "rgba(153, 102, 255)",
+        "rgba(255, 159, 64)",
       ],
       borderColor: [
         "rgba(255, 99, 132, 1)",
--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import RechartLineGraph from "./rechartLine";
 import SimplePie from "./pie";
+import SimpleDoughnut from "./doughnut";
 const Scene = () => {
   return (
     <div>
       <RechartLineGraph />
       <SimplePie />
+      <div className="aspect-square w-[500px]">
+        <SimpleDoughnut />
+      </div>
     </div>
   );
 };
