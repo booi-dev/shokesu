adc214e8d6d16c0d9e1ff681fb79e8af439082d2
booi-dev <booimangang@gmail.com>
2023-08-17 23:05:58 +0530

    Create polar area chart

--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import RechartLineGraph from "./rechartLine";
 import SimplePie from "./pie";
 import SimpleDoughnut from "./doughnut";
+import SimplePolarArea from "./polarAreaChart";
 const Scene = () => {
   return (
     <div>
-      <div className="mx-auto flex aspect-square w-[500px] flex-col gap-4">
+      <div className="mx-auto flex aspect-square w-full max-w-[500px] flex-col gap-4">
         <SimplePie />
         <SimpleDoughnut />
+        <SimplePolarArea />
       </div>
     </div>
   );
