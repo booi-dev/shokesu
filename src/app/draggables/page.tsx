303a95d77ed63433a01bb3084834435ce14a5570
booi-dev <booimangang@gmail.com>
2023-09-23 15:57:37 +0530

    Fix dragging calculation

--- src/app/draggables/page.tsx
+++ src/app/draggables/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import Image from "next/image";
 
 const ResizableDiv: React.FC = () => {
-  const [width, setWidth] = useState<number>(250); // Initial width in pixels
+  const [width, setWidth] = useState<number>(250);
+  const [draggerLeft, setDraggerLeft] = useState(100); // Initial width in pixels
   const [isResizing, setIsResizing] = useState<boolean>(false);
   const [startX, setStartX] = useState<number>(0);
 
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
   const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
     setIsResizing(true);
-    setStartX(e.clientX);
+    // setStartX(e.clientX);
     e.currentTarget.style.transition = "none";
-    console.log("mouse down");
+    // console.log("mouse down");
   };
 
   const handleMouseMove = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -20,38 +23,60 @@ const ResizableDiv: React.FC = () => {
 
     const offsetX = e.clientX - startX;
     const newWidth = width + offsetX;
+    const neu = draggerLeft + offsetX;
 
-    setWidth(newWidth);
+    // setWidth(newWidth);
+    console.log(e.clientX);
+    setDraggerLeft(offsetX);
 
     console.log("mouse move");
   };
 
-  const handleMouseUp = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
     if (isResizing) {
       setIsResizing(false);
       e.currentTarget.style.transition = "left 0.3s ease"; // Add smooth transition
     }
 
-    console.log("mouse up");
+    // console.log("mouse up");
   };
+
+  const handleMouseClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const offsetX = e.clientX - startX;
+    const newWidth = width + offsetX;
+
+    setWidth(newWidth);
+  };
+
+  //   const handleMouseLeave = () => {
+  //     setIsResizing(false);
+  //     console.log("mouse leave");
+  //   };
+
+  useLayoutEffect(() => {
+    if (containerRef.current) {
+      const containerPos = containerRef.current.getBoundingClientRect();
+      setStartX(containerPos.x);
+      console.log(containerPos.x);
+    }
+  }, []);
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className=" aspect-square w-[500px] bg-green-200">
+      <div
+        ref={containerRef}
+        className=" aspect-square w-[500px] bg-green-200"
+        onMouseUp={handleMouseUp}
+        // onClick={handleMouseClick}
+      >
         <div className="relative h-full w-full bg-green-400" style={{ width }}>
           <button
-            className="absolute -right-4 top-1/2 -translate-y-1/2"
+            className="absolute -right-[1.2rem] top-1/2 h-full w-[40px] origin-center -translate-x-1/2 -translate-y-1/2 bg-red-500"
             onMouseDown={handleMouseDown}
             onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
-          >
-            <Image
-              src="/icons/drag-vertical.svg"
-              width={30}
-              height={30}
-              alt="Resizable Button"
-            />
-          </button>
+            // onMouseUp={handleMouseUp}
+            style={{ left: draggerLeft }}
+          ></button>
         </div>
       </div>
     </div>
